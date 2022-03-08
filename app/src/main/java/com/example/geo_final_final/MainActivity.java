0e892886afeb5d1851a8062fb687b56ac6567a43
Zakaria-ElGhoul <30044@ma-web.nl>
2022-03-08 10:33:44 +0100

    Added distance

--- app/src/main/java/com/example/geo_final_final/MainActivity.java
+++ app/src/main/java/com/example/geo_final_final/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.geo_final_final;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -8,6 +9,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,17 +25,16 @@ public class MainActivity extends AppCompatActivity {
     double longitude;
     double latitude;
     String userCountry, userAddress;
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Button button = (Button)findViewById(R.id.button);
         TextView tv = findViewById(R.id.text_view);
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -66,17 +67,15 @@ public class MainActivity extends AppCompatActivity {
             longitude = 0.0;
         }
 
-
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE}, 1);
 
         try {
-
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
             if (addresses != null && addresses.size() > 0) {
                 userCountry = addresses.get(0).getCountryName();
                 userAddress = addresses.get(0).getAddressLine(0);
-                tv.setText(userCountry + ", " + userAddress);
+                tv.setText(userCountry + "\n" + userAddress + '\n' + longitude  + '\n' + latitude + "\n" + "Distance = " + distFrom(latitude, longitude, 52.371675, 4.910954));
             }
             else {
                 userCountry = "Unknown";
@@ -86,5 +85,40 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        button.setOnClickListener(view -> {
+            if (gps_loc != null) {
+                latitude = final_loc.getLatitude();
+                longitude = final_loc.getLongitude();
+            }
+            else if (network_loc != null) {
+                latitude = final_loc.getLatitude();
+                longitude = final_loc.getLongitude();
+            }
+            else {
+                latitude = 0.0;
+                longitude = 0.0;
+            }
+            tv.setText(userCountry + "\n" + userAddress + '\n' + longitude  + '\n' + latitude + "\n" + "Distance = " + distFrom(latitude, longitude, 52.371675, 4.910954));
+        });
+
+    }
+
+
+    public static float distFrom(double lat1, double lng1, double lat2, double lng2)
+    {
+        // Earth Radius in meters
+        double earthRadius = 6371000;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) *
+                        Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        float dist = (float) (earthRadius * c);
+
+        System.out.println("Distance is : " + lat1);
+        System.out.println("Distance is : " + lat2);
+        return dist; // distance in meters
     }
 }
